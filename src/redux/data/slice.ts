33272c58f2677e2b7d7d129378d3616f2321ff9f
MarkHW <lolka-pop@rambler.ru>
2023-02-25 20:30:15 +0300

    refact

--- src/redux/data/slice.ts
+++ src/redux/data/slice.ts
@@ -1,19 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// type gameItems = {
-//   value: number;
-// };
+type gameItems = {
+  value: number;
+};
 
-// type dataItem = {
-//   gameBoard: gameItems[];
-// };
+type dataItem = {
+  gameBoard: gameItems[][];
+  lastWinner: number;
+};
 
-const initialState = {
+const initialState: dataItem = {
   gameBoard: [
     [{ value: 0 }, { value: 0 }, { value: 0 }],
     [{ value: 0 }, { value: 0 }, { value: 0 }],
     [{ value: 0 }, { value: 0 }, { value: 0 }],
   ],
+  lastWinner: 0,
 };
 
 const dataSlice = createSlice({
@@ -30,7 +32,6 @@ const dataSlice = createSlice({
       let cell = state.gameBoard;
       let vert1 = 1;
       let vert2 = 1;
-      let win = "";
 
       for (let i = 0; i < 3; i++) {
         vert1 = vert1 * cell[i][i].value;
@@ -42,17 +43,16 @@ const dataSlice = createSlice({
           hor2 = hor2 * cell[j][i].value;
         }
         if ((hor1 || hor2) === 1) {
-          win = "1!";
+          state.lastWinner = 1;
         } else if ((hor1 || hor2) === 8) {
-          win = "2!";
+          state.lastWinner = 2;
         }
       }
       if ((vert1 || vert2) === 1) {
-        win = "1!";
+        state.lastWinner = 1;
       } else if ((vert1 || vert2) === 8) {
-        win = "2!";
+        state.lastWinner = 2;
       }
-      console.log(win);
     },
   },
 });
