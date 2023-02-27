d40196d793e6abceb3598ec269216a99f3990b7b
MarkHW <lolka-pop@rambler.ru>
2023-02-27 16:57:06 +0300

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { reset, endGame, closeActiveStatus } from "./redux/data/slice";
 import { RootState, useAppDispatch } from "./redux/store";
 import rest from "./assets/img/Redo.svg";
 import X from "./assets/img/Combined Shape Copy 2.svg";
+import xGray from "./assets/img/x-gray.svg";
 import O from "./assets/img/Oval.svg";
 
 const App: React.FC = () => {
@@ -24,28 +25,35 @@ const App: React.FC = () => {
     user === false ? setUser(true) : setUser(false);
   }
 
-  const restart = () => {
-    if (endRound) {
-      dispatch(reset());
-      setUser(false);
-    }
-  };
-  restart();
+  useEffect(() => {
+    const restart = () => {
+      if (endRound) {
+        dispatch(reset());
+        setUser(false);
+      }
+    };
+    restart();
+  }, [endRound]);
 
   return (
     <div className="App">
       <div className="container">
         <div className="display-wrapper">
-          <div>
-            <img src={X} />
-            <img src={O} />
+          <div className="display__players">
+            <img src={X} className="display__players-img" />
+            <img src={O} className="display__players-img" />
           </div>
-          <div>
-            <img src={user ? O : X} />
-            <span>TURN</span>
+          <div className="display__status">
+            <div className="display__status-content">
+              <img src={user ? O : xGray} className="display__status-img" />
+              <div>TURN</div>
+            </div>
           </div>
-          <button onClick={() => dispatch(endGame())}>
-            <img src={rest} />
+          <button
+            onClick={() => dispatch(endGame())}
+            className="button__restart"
+          >
+            <img src={rest} className="button__restart-img" />
           </button>
         </div>
         <div className="game">
@@ -89,39 +97,51 @@ const App: React.FC = () => {
           </>
         </div>
         <div className="scoreboard">
-          <div>
+          <div className="display display-blue ">
             <span>X (YOU)</span>
-            <span>{playerXCount}</span>
+            <span className="display__value">{playerXCount}</span>
           </div>
 
-          <div>
+          <div className="display display-grey">
             <span>TIES</span>
-            <span>{roundCount}</span>
+            <span className="display__value">{roundCount}</span>
           </div>
-          <div>
+          <div className="display display-orange">
             <span>O (CPU)</span>
-            <span>{playerOCount}</span>
+            <span className="display__value">{playerOCount}</span>
           </div>
         </div>
         {winner === 1 && (
           <div
             className={
-              statusActive ? "winner-container" : `winner-container hide`
+              statusActive
+                ? `winner-container-background`
+                : `winner-container-background hide`
             }
           >
-            <div className="winner">
-              <p>OH NO, O Win…</p>
-              <h1>TAKES THE ROUND</h1>
-              <div>
-                <button>QUIT</button>
-                <button onClick={() => dispatch(closeActiveStatus())}>
-                  NEXT ROUND
-                </button>
+            <div className="winner-container-wrapper">
+              <div className="winner-container">
+                <p className="winner-container__status">OH NO, YOU LOST…</p>
+                <div className="winner-container__subject">
+                  <img src={O} className="winner-container__subject-img" />
+                  <h1 className="winner-container__subject-title">
+                    TAKES THE ROUND
+                  </h1>
+                </div>
+                <div className="winner-container__buttons">
+                  <button className="button__winner-gray ">QUIT</button>
+                  <button
+                    className="button__winner-orange"
+                    onClick={() => dispatch(closeActiveStatus())}
+                  >
+                    NEXT ROUND
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         )}
-        {winner === 2 && (
+        {/* {winner === 2 && (
           <div
             className={
               statusActive ? "winner-container" : `winner-container hide`
@@ -138,7 +158,7 @@ const App: React.FC = () => {
               </div>
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
